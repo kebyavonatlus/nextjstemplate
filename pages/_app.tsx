1afd6c18e91a26698eeb7c3e2be37f1b27e3c1c4
Aybek Sultanov <aybek-sultanov@inbox.ru>
2022-03-07 12:39:15 +0600

    добавить шрифты, удалить лишние компоненты

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,11 +5,13 @@ import '../styles/globals.css';
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return <div>
     <Head>
-        <title>MyTop - наш лучший топ</title>
-        <link key={1} rel="icon" href="/favicon.ico" />
-      </Head>
+      <title>MyTop - наш лучший топ</title>
+      <link key={1} rel="icon" href="/favicon.ico" />
+      <link rel="preconnect" href="https://gonts.gstatic.com" />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap" />
+    </Head>
     <Component {...pageProps} />;
-    </div>;
+  </div>;
 }
 
 export default MyApp;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,10 @@
 import Document, { DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
-    
+
     static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
         const initialProps = await Document.getInitialProps(ctx);
-        return {...initialProps};
+        return { ...initialProps };
     }
 
     render(): JSX.Element {
