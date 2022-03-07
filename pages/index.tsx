0c06f24f7e6752a7504932d003b8fc0a5503a2fd
Aybek Sultanov <aybek-sultanov@inbox.ru>
2022-03-07 17:08:14 +0600

    add svg

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home(): JSX.Element {
   return (
     <>
       <Htag tag='h1'>Тест</Htag>
-      <Button appearance="primary" className="asdasdasd">Кнопка</Button>
+      <Button appearance="primary" arrow="right" className="asdasdasd">Кнопка</Button>
       <Button appearance="ghost">Кнопка</Button>
     </>
   );
