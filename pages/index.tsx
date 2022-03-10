5f5e1b42d7899a4a90d54ccda69f2ad1808f50ad
Aybek Sultanov <aybek-sultanov@inbox.ru>
2022-03-10 10:29:20 +0600

    add new component Tag href

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Htag, P } from "../components";
+import { Button, Htag, P, Tag } from "../components";
 
 export default function Home(): JSX.Element {
   return (
@@ -9,6 +9,10 @@ export default function Home(): JSX.Element {
       <P size="l">Большой</P>
       <P>Средний</P>
       <P size="s">Маленький</P>
+      <Tag size="s">Маленький</Tag>
+      <Tag size="m" color="red">RED</Tag>
+      <Tag size="s" color="green">GREEN</Tag>
+      <Tag color="primary">Маленький</Tag>
     </>
   );
 }
