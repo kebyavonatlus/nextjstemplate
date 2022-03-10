dff941f0c8c55f5f31ccd4874c39a48761ae0862
Aybek Sultanov <aybek-sultanov@inbox.ru>
2022-03-10 09:38:45 +0600

    add new config for next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
 
-module.exports = nextConfig;
+module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        // test: /\.(js|ts)x?$/,
+       // for webpack 5 use
+       and: [/\.(js|ts)x?$/]
+      },
+      
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+};
